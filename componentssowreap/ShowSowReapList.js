c0fa564ec2ff0e87f1be3ff91af716334d8fa576
Joy Carlson <63269555+joynewwork@users.noreply.github.com>
2020-12-15 13:37:37 -1000

    Add files via upload

--- componentssowreap/ShowSowReapList.js
+++ componentssowreap/ShowSowReapList.js
@@ -1,71 +1,107 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import '../App.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import SowReapCard from './SowReapCard';
+import Moment from 'moment';
 
-class ShowSowReapList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        sowreaps: []
-    };
-  }
+function ShowSowReapList() {
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:3001/sowreap')
-      .then(res => {
-        this.setState({
-            sowreaps: res.data
-        })
-      })
-      .catch(err =>{
-        console.log('Error from ShowSowReapList');
-      })
-  };
+  var [isEdit, setIsEdit] = useState("");
+  var [sowreaps, setSowReaps] = useState([]);
+  var [sowreap, setSowReap] = useState({});
+
+  var [date, setDate] = useState("");
+  var [believingfor, setBelievingfor] = useState("");
+  var [scriptures, setScriptures] = useState("");
+  var [dailydeclaration, setDailydeclaration] = useState("");
+  var [planofaction, setPlanofaction] = useState("");
+  var [answerarrived, setAnswerarrived] = useState("");
 
 
-  render() {
-    const sowreaps = this.state.sowreaps;
-    console.log("PrintSowReap: " + sowreaps);
-    let sowreapList;
+  useEffect(() => {
+    const getAllSowReaps = async () => {
+      let sowreapsData = await fetch('http://localhost:3001/sowreaps/')
+      let swrp = await sowreapsData.json();
 
-    if(!sowreaps) {
-        sowreapList = "there is no sow and reap record!";
-    } else {
-        sowreapList = sowreaps.map((sowreap, k) =>
-        <SowReapCard sowreap={sowreap} key={k} />
-      );
+      console.log(swrp);
+
+      setSowReaps(swrp.data.sowreaps);
+
     }
+    getAllSowReaps();
+  }, []);
 
-    return (
-      <div className="ShowSowReapList">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <br />
-              <h2 className="display-4 text-center">Sow and Reap List</h2>
-            </div>
 
-            <div className="col-md-11">
-              <Link to="/create-sowreap" className="btn btn-outline-blue float-right">
-                + Add New Sow and Reap Item
-              </Link>
-              <br />
-              <br />
-              <hr />
-            </div>
-
-          </div>
-
-          <div className="list">
-                {sowreapList}
-          </div>
-        </div>
+  return (
+    <div className="ShowSowReapList">
+      <div className="container">
+      <div className="col-md-12">
+        <br />
+        <h2 className="display-4 text-center">Sow and Reap List</h2>
       </div>
-    );
-  }
+      <div className="col-md-11">
+        <Link to="/CreateSowReap" className="btn btn-outline-blue float-right">
+          + Add New Sowing and Reapng Item
+              </Link>
+        <br />
+        <br />
+        <hr />
+      </div>
+      <div>
+
+      </div>
+      {sowreaps.map((sowreap, idx) => {
+        return (
+
+          <div key={idx}>
+            <table className='list-container'>
+
+              <tr>
+                <td width='100px'>
+                {Moment(sowreap.date).format("MM-DD-YYYY")}
+                </td>
+                <td width='50px'>
+                  {sowreap.believingfor}
+                </td>
+                <td width='90px'>
+                  {sowreap.scriptures}
+                </td>
+                <td width='90px'>
+                  {sowreap.dailydeclaration}
+                </td>
+                <td width='90px'>
+                  {sowreap.planofaction}
+                </td>
+                <td width='100px'>
+                {Moment(sowreap.answerarrived).format("MM-DD-YYYY")}
+                </td>
+              </tr>
+            </table>
+
+          </div>
+
+
+        )
+      })};
+      </div>
+    </div>
+
+
+
+
+  )
 }
 
+
+
+
 export default ShowSowReapList;
+
+
+
+
+
+
+
+
