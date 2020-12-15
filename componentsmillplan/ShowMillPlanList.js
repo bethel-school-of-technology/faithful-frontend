2bae2e8abc29e6323b867db8c68bd6d46c419ba4
Joy Carlson <63269555+joynewwork@users.noreply.github.com>
2020-12-15 13:30:36 -1000

    Add files via upload

--- componentsmillplan/ShowMillPlanList.js
+++ componentsmillplan/ShowMillPlanList.js
@@ -1,78 +1,102 @@
-import React, { Component, useState, useEffect } from 'react';
+/*import React, { Component, useState, useEffect } from 'react';
 import '../App.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import MillPlanCard from './MillPlanCard';
 
-function App() {}
 
-class ShowMillPlanList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        millplans: []
-    };
-  }
+function ShowMillPlanList() {
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:3001/millplan')
-      .then(res => {
-        this.setState({
-            millplans: res.data
-        })
-      })
-      .catch(err =>{
-        console.log('Error from ShowMillPlanList');
-      })
-  };
+  var [isEdit, setIsEdit] = useState("");
+  var [millplans, setMillPlans] = useState([]);
+  var [millplan, setMillPlan] = useState({});
 
+  var [timeline, setTimeline] = useState("");
+  var [category, setCategory] = useState("");
+  var [description, setDescription] = useState("");
+  var [cost, setCost] = useState(-1);
+  var [comments, setComments] = useState("");
 
-  render() {
-    const millplans = this.state.millplans;
-    console.log("PrintMillPlan: " + millplans);
-    let millplanList;
-    
+  useEffect(() => {
+    const getAllMillPlans = async () => {
+      let millplansData = await fetch('http://localhost:3001/millplans/')
+      let mllpln = await millplansData.json();
 
-    if(!millplans) {
-        millplanList = "there is no dream and vision record!";
-    } else {
-        millplanList = millplans.map((millplan, k) =>
-        <MillPlanCard millplan={millplan} key={k} />
-      );
+      console.log(mllpln);
+
+      setMillPlans(mllpln.data.millplans);
+
     }
-   
+    getAllMillPlans();
+  }, []);
 
 
-    return (
-      <div className="ShowMillPlanList">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <br />
-              <h2 className="display-4 text-center">Million Dollar Plan List</h2>
-            </div>
-            <div>
-            
-            </div>
-            <div className="col-md-11">
-              <Link to="/create-millplan" className="btn btn-outline-blue float-right">
-                + Add New Million Dollar Plan Cash Flow Item
-              </Link>
-              <br />
-              <br />
-              <hr />
-            </div>
-
-          </div>
-
-          <div className="list">
-                {millplanList}
-          </div>
-        </div>
+  return (
+    <div className="ShowMillPlanList">
+    <div className="container">
+      <div className="col-md-12">
+        <br />
+        <h2 className="display-4 text-center">Million Dollar Plan List</h2>
       </div>
-    );
-  }
+      <div className="col-md-11">
+        <Link to="/CreateMillPlan" className="btn btn-outline-blue float-right">
+          + Add New Item to the Million Dollar Plan
+              </Link>
+        <br />
+        <br />
+        <hr />
+      </div>
+      <div>
+
+      </div>
+      {millplans.map((millplan, idx) => {
+        return (
+
+          <div key={idx}>
+            <table className='list-container'>
+
+              <tr>
+                <td width='90px'>
+                  {millplan.timeline}
+                </td>
+                <td width='90px'>
+                  {millplan.category}
+                </td>
+                <td width='50px'>
+                  {millplan.description}
+                </td>
+                <td width='50px'>
+                  {millplan.cost}
+                </td>
+                <td width='50px'>
+                  {millplan.comments}
+                </td>
+              </tr>
+            </table>
+
+          </div>
+
+
+        )
+      })};
+      </div>
+    </div>
+
+
+
+
+  )
 }
 
-export default ShowMillPlanList;
+
+
+
+export default ShowMillPLanList;
+
+
+*/
+
+
+
+
+
